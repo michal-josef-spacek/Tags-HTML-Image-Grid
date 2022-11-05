faa6ee72a3b5dbf6665f787a49fe3a2aa45edc54
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-11-05 17:33:33 +0100

    Prepare for 0.03 version

--- Grid.pm
+++ Grid.pm
@@ -10,7 +10,7 @@ use List::MoreUtils qw(none);
 use Scalar::Util qw(blessed);
 use Unicode::UTF8 qw(decode_utf8);
 
-our $VERSION = 0.02;
+our $VERSION = 0.03;
 
 # Constructor.
 sub new {
@@ -506,6 +506,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.02
+0.03
 
 =cut
